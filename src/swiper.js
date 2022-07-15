36912fb335e75bc6faeecadbb664b82b30d54456
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-07-15 22:36:59 +0300

    Update swiper.js

--- src/swiper.js
+++ src/swiper.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
         loop: true,
         grabCursor: true,
+        longSwipes: false,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
