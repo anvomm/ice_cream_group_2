fa5f34858e9e2d47ec10d73d07c950fc5bea2923
anvomm <104103036+anvomm@users.noreply.github.com>
2022-07-16 10:13:18 +0000

    Deploying to gh-pages from @ anvomm/ice_cream_group_2@49df40cf925975a0e829485e9cda9e2d37843f4c 🚀

---
+++ index.44101707.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{loop:!0,grabCursor:!0,longSwipes:!1,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.44101707.js.map
--- index.cb58d8ac.js
+++ index.cb58d8ac.js
@@ -1,2 +1,2 @@
 var swiper=new Swiper(".mySwiper",{loop:!0,grabCursor:!0,longSwipes:!1,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.a836c0dc.js.map
+//# sourceMappingURL=index.cb58d8ac.js.map
