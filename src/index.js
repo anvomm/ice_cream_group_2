b04ba35920ecaec925fb4fee82dca565c3967ced
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-07-16 14:14:44 +0300

    conflict fixed

--- src/index.js
+++ src/index.js
@@ -6,4 +6,20 @@ var swiper = new Swiper(".mySwiper", {
           el: ".swiper-pagination",
           clickable: true,
         },
-      });
+});
+ 
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("modal-hidden");
+    }
+  })();
