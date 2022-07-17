aab9ae4a054de7395edd9cf94d8da82b6d2116cc
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-07-17 12:21:39 +0300

    js update

--- src/js/mobile_modal.js
+++ src/js/mobile_modal.js
@@ -6,7 +6,6 @@
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("modal-hidden");
