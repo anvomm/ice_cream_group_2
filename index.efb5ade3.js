8a309cea719ac0b2b71515e28c74e46029581b43
anvomm <104103036+anvomm@users.noreply.github.com>
2022-07-17 14:31:51 +0000

    Deploying to gh-pages from @ anvomm/ice_cream_group_2@5f6ed9c718d2c97f9349fba9eda7c620d770053d 🚀

--- index.efb5ade3.js
+++ index.efb5ade3.js
@@ -1,2 +1,2 @@
 (()=>{const e={openModalBtn:document.querySelector("[data-window-open]"),closeModalBtn:document.querySelector("[data-window-close]"),modal:document.querySelector("[data-window]")};function o(){e.modal.classList.toggle("modal-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.266be4e2.js.map
+//# sourceMappingURL=index.efb5ade3.js.map
