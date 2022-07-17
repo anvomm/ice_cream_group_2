bbe4db3287a4f9db73cbb490aab9cad59a830811
anvomm <104103036+anvomm@users.noreply.github.com>
2022-07-17 22:21:25 +0000

    Deploying to gh-pages from @ anvomm/ice_cream_group_2@952a1434e3df147ce7d72aca6fa5704bfa12b329 🚀

---
+++ index.971cf14c.js
@@ -0,0 +1,2 @@
+const anchors=document.querySelectorAll("a.scroll-to");for(let t of anchors)t.addEventListener("click",(function(e){e.preventDefault();const o=t.getAttribute("href");document.querySelector(o).scrollIntoView({behavior:"smooth",block:"start"})}));
+//# sourceMappingURL=index.971cf14c.js.map
