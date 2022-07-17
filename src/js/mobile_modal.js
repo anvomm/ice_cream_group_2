6444bc3cf6eb638f0c7fd5b7c5fffbe193ec8f40
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-07-17 11:35:07 +0300

    modal switched

---
+++ src/js/mobile_modal.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-mobmodal-open]"),
+      closeModalBtn: document.querySelector("[data-mobmodal-close]"),
+      modal: document.querySelector("[data-mobmodal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("modal-hidden");
+    }
+  })();
