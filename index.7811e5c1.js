6ad789438620c4fbf433cd570e342d4ef94e7f53
anvomm <104103036+anvomm@users.noreply.github.com>
2022-07-18 22:56:11 +0000

    Deploying to gh-pages from @ anvomm/ice_cream_group_2@f4b4bb74ea7fa43ff26da8d168406bcc7ff560d7 🚀

---
+++ index.7811e5c1.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-map-open]"),closeModalBtn:document.querySelector("[data-map-close]"),modal:document.querySelector("[data-map]")};function a(){e.modal.classList.toggle("modal-hidden")}e.openModalBtn.addEventListener("click",a,{passive:!0}),e.closeModalBtn.addEventListener("click",a,{passive:!0})})();
+//# sourceMappingURL=index.7811e5c1.js.map
